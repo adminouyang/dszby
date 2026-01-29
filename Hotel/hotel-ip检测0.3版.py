1591dd3278b40adeae917d1f18e8097ef5bf9105
231006 <109812774+adminouyang@users.noreply.github.com>
2026-01-29 12:05:25 +0800

    Update hotel-ip检测0.3版.py

--- Hotel/hotel-ip检测0.3版.py
+++ Hotel/hotel-ip检测0.3版.py
@@ -99,10 +99,10 @@ SPEED_THRESHOLD = 0.1
 
 # User-Agent列表
 USER_AGENTS = [
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2 Safari/605.1.15"
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+    # "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0",
+    # "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)",
+    # "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2 Safari/605.1.15"
 ]
 
 # 频道分类定义
